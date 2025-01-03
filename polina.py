6f2508ddcb19053be37551e5e84f2b4e6639dd88
chaos-soft <mail@57st.su>
2025-01-03 21:31:14 +0300

    Обновление polina
    append_once для добавления только в случае отсутствия файла;
    insert заменён на insert_after (после конкретной строки);
    добавлена сверка файлов с резервной копией (check_backup);
    переделан формат JSON;
    убран реверс;
    убрана привязка к polina_config.json.

--- polina.py
+++ polina.py
@@ -1,140 +1,117 @@
 #!/usr/bin/env python3
 from pathlib import Path
+from typing import Any
 import argparse
+import filecmp
 import json
 import subprocess
 import sys
 
 BASE_DIR = Path(__file__).resolve().parent
-APPEND: dict[str, list[str]] = {}
 BACKUP_PATH: Path = BASE_DIR / 'backup'
-FILES: set[str] = set()
-INSERT: dict[str, dict[str, list[str]]] = {}
-IS_REVERSE: bool = False
-REPLACE: dict[str, list[list[str]]] = {}
+CONFIG: dict[str, Any] = {}
 VARIABLES: dict[str, list[str]] = {}
 
 
-def add2replace(k, list_) -> None:
-    if k in REPLACE:
-        REPLACE[k].extend(list_)
-    else:
-        REPLACE[k] = list_
+def append(file_path: str, list_: list[str]) -> None:
+    str_ = '\n'.join(list_)
+    str_ = replace_variables(str_)
+    print(f'append {file_path}')
+    Path(file_path).parent.mkdir(mode=0o755, parents=True, exist_ok=True)
+    with open(file_path, 'a') as f:
+        f.write(f'{str_}\n')
 
 
-def append() -> None:
-    v: list[str]
-    for file_path, v in APPEND.items():
-        str_ = '\n'.join(v)
-        str_ = replace_variables(str_)
-        print(f'append {file_path}')
-        Path(file_path).parent.mkdir(mode=0o755, parents=True, exist_ok=True)
-        with open(file_path, 'a') as f:
-            f.write(f'{str_}\n')
+def append_once(file_path: str, list_: list[str]) -> None:
+    if not Path(file_path).exists():
+        append(file_path, list_)
 
 
 def backup() -> None:
     BACKUP_PATH.mkdir(mode=0o755, parents=True, exist_ok=True)
-    for file_path in FILES:
-        fp = Path(file_path)
-        if fp.exists() and not (BACKUP_PATH / get_backup_name(file_path)).exists():
+    for file_path in CONFIG.keys():
+        if Path(file_path).exists() and not (BACKUP_PATH / get_backup_name(file_path)).exists():
             print(f'backup {file_path}')
             subprocess.run(['cp', '-rp', file_path, BACKUP_PATH / get_backup_name(file_path)], check=True)
+
+
+def check_backup() -> None:
+    for file_path in CONFIG.keys():
+        bp = BACKUP_PATH / get_backup_name(file_path)
+        if Path(file_path).exists() and bp.exists():
+            if not filecmp.cmp(file_path, bp, shallow=True):
+                print(f'not equal {file_path}')
 
 
 def get_backup_name(file_path: str) -> str:
     return file_path.replace('/', '__').lstrip('__')
 
 
-def insert() -> None:
-    v: dict[str, list[str]]
-    for file_path, v in INSERT.items():
-        if Path(file_path).exists():
-            print(f'insert {file_path}')
-            with open(file_path, 'r+') as f:
-                data = f.readlines()
-                for k, vv in v.items():
-                    data.insert(int(k) - 1, '{}\n'.format('\n'.join(vv)))
-                f.seek(0)
-                f.writelines(data)
+def insert_after(file_path: str, dict_: dict[str, list[str]]) -> None:
+    if Path(file_path).exists():
+        print(f'insert_after {file_path}')
+        with open(file_path, 'r+') as f:
+            data = f.readlines()
+            for k, v in dict_.items():
+                try:
+                    i = data.index(k)
+                    data.insert(i + 1, '{}\n'.format('\n'.join(v)))
+                except ValueError:
+                    pass
+            f.seek(0)
+            f.writelines(data)
 
 
-def load_config() -> None:
-    global APPEND, COMMANDS, FILES, INSERT, REPLACE, VARIABLES
-    with open(BASE_DIR / 'polina_config.json') as f:
-        data = json.load(f)
-    if IS_REVERSE:
-        if 'variables' in data:
-            VARIABLES |= data['variables']
-        if 'append' in data:
-            for k, v in data['append'].items():
-                str_ = '\n'.join(v)
-                str_ = replace_variables(str_)
-                list_ = [[f'{str_}\n', '']]
-                add2replace(k, list_)
-            FILES |= set(REPLACE.keys())
-        if 'insert' in data:
-            for k, v in data['insert'].items():
-                list_ = [['{}\n'.format('\n'.join(vv)), ''] for vv in v.values()]
-                add2replace(k, list_)
-            FILES |= set(REPLACE.keys())
-        if 'replace' in data:
-            for k, v in data['replace'].items():
-                list_ = [[vv[1], vv[0]] for vv in v]
-                add2replace(k, list_)
-            FILES |= set(REPLACE.keys())
-        if 'remove' in data:
-            APPEND |= data['remove']
-            FILES |= set(APPEND.keys())
-        return None
-    if 'append' in data:
-        APPEND |= data['append']
-        FILES |= set(APPEND.keys())
-    if 'insert' in data:
-        INSERT |= data['insert']
-        FILES |= set(INSERT.keys())
-    if 'replace' in data:
-        REPLACE |= data['replace']
-        FILES |= set(REPLACE.keys())
-    if 'variables' in data:
-        VARIABLES |= data['variables']
-    if 'remove' in data:
-        for k, v in data['remove'].items():
-            list_ = [[vv, ''] for vv in v]
-            add2replace(k, list_)
-        FILES |= set(REPLACE.keys())
+def load_config(file: str) -> None:
+    global CONFIG, VARIABLES
+    with open(file) as f:
+        CONFIG = json.load(f)
+        if CONFIG.get('variables'):
+            VARIABLES = CONFIG.pop('variables')
 
 
 def rebuild() -> None:
     reset()
-    append()
-    insert()
-    replace()
+    for file_path, actions in CONFIG.items():
+        if 'append' in actions:
+            append(file_path, actions['append'])
+        if 'append_once' in actions:
+            append_once(file_path, actions['append_once'])
+        if 'insert_after' in actions:
+            insert_after(file_path, actions['insert_after'])
+        if 'remove' in actions:
+            list_ = [[v, ''] for v in actions['remove']]
+            if 'replace' in actions:
+                actions['replace'].extend(list_)
+            else:
+                actions['replace'] = list_
+        if 'replace' in actions:
+            replace(file_path, actions['replace'])
 
 
-def replace() -> None:
-    v: list[list[str]]
-    for file_path, v in REPLACE.items():
-        if Path(file_path).exists():
-            print(f'replace {file_path}')
-            with open(file_path, 'r+') as f:
-                data = f.read()
-                for vv in v:
-                    data = data.replace(vv[0], vv[1])
-                f.seek(0)
-                f.write(data)
-                f.truncate()
+def replace(file_path: str, list_: list[list[str]]) -> None:
+    if Path(file_path).exists():
+        print(f'replace {file_path}')
+        with open(file_path, 'r+') as f:
+            data = f.read()
+            for v in list_:
+                data = data.replace(v[0], v[1])
+            f.seek(0)
+            f.write(data)
+            f.truncate()
 
 
 def replace_variables(str_: str) -> str:
-    for var, v in VARIABLES.items():
+    for var, list_ in VARIABLES.items():
         if var in str_:
-            str_ = str_.replace(var, '\n'.join(v))
+            str_ = str_.replace(var, '\n'.join(list_))
     return str_
 
 
 def reset() -> None:
-    for file_path in FILES:
+    for file_path, actions in CONFIG.items():
+        if 'append_once' in actions:
+            continue
         fp = Path(file_path)
         bp = BACKUP_PATH / get_backup_name(file_path)
         if bp.exists():
@@ -148,9 +125,10 @@ def reset() -> None:
 
 
 if __name__ == '__main__':
-    load_config()
     parser = argparse.ArgumentParser()
-    parser.add_argument('action', choices=['backup', 'rebuild', 'reset'], type=str)
+    parser.add_argument('config_file', type=str)
+    parser.add_argument('action', choices=['backup', 'check_backup', 'rebuild', 'reset'], type=str)
     args = parser.parse_args()
+    load_config(args.config_file)
     eval(f'{args.action}()')
     sys.exit(0)
